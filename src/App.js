2100ebd6eb17d6f77cc37f34d3c13017787a1858
Minakshi <jhaminakshi333@gmail.com>
2023-09-23 23:24:21 +0545

    adding card to newsitems

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Navbar from './components/Navbar'
 import News from './components/News'
 
 export default class App extends Component {
   render() {
     return (
       <div>
+      <Navbar/>
         <h1>news app</h1>
         <News/>
       </div>
--- src/components/News.js
+++ src/components/News.js
@@ -5,9 +5,21 @@ export default class News extends Component {
   render() {
     return (
       <div>
-      <h3>news to be shown</h3>
-        <NewsItems/>
+        <div className="container">
+          <h3>365News-Top Headlines</h3>
+          <div className="row">
+            <div className="col-md-4">
+              <NewsItems title="news details" description="news description" />
+            </div>
+            <div className="col-md-4">
+              <NewsItems title="news details" description="news description" />
+            </div>
+            <div className="col-md-4">
+              <NewsItems title="news details" description="news description" />
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
