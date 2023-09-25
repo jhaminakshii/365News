0b3e1716eebaeb02ed3558c746f14277774e0ac8
Minakshi <jhaminakshi333@gmail.com>
2023-09-25 22:18:33 +0545

    adding top-loading-bar

--- src/components/News.js
+++ src/components/News.js
@@ -43,10 +43,14 @@ export default class News extends Component {
         //}
 
        async componentDidMount(){
+            this.props.setProgress(10);
             let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=53313403d76f4152ab061ff17a6843f5&page=1&pageSize=${this.props.pageSize}`;
             let data =await fetch(url);
+            this.props.setProgress(30);
             let parsedData = await data.json();
+            this.props.setProgress(70);
             this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults,loading: false})
+            this.props.setProgress(100);
         }
         //  handlePrevClick=async()=>{
         //     // console.log("prev");
