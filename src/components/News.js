02b52004ff76afa96b78c3ef95851aa64005c09a
Minakshi <jhaminakshi333@gmail.com>
2023-09-25 17:18:25 +0545

    adding time,author and source name in news card

--- src/components/News.js
+++ src/components/News.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types'
 export default class News extends Component {
     static defaultProps={
       country:"in",
-      pageSize:8,
+      pageSize:9,
       category:'general'
     }
     static propTypes ={
@@ -24,6 +24,18 @@ export default class News extends Component {
               page:1
             };
         }
+        // updateNew = async()=>{
+        //   let url =
+        //       `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=53313403d76f4152ab061ff17a6843f5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        //     this.setState({ loading: true });
+        //     let data = await fetch(url);
+        //     let parsedData = await data.json();
+        //     this.setState({
+        //       articles: parsedData.articles,
+        //       loading: false
+        //     });
+        //}
+
        async componentDidMount(){
             let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=53313403d76f4152ab061ff17a6843f5&page=1&pageSize=${this.props.pageSize}`;
             let data =await fetch(url);
@@ -31,7 +43,9 @@ export default class News extends Component {
             this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults,loading: false})
         }
          handlePrevClick=async()=>{
-            console.log("prev");
+            // console.log("prev");
+            // this.setState({page:this.state.page-1})
+            // this.updateNew();
             let url =
               `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=53313403d76f4152ab061ff17a6843f5&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
             this.setState({ loading: true });
@@ -45,9 +59,7 @@ export default class News extends Component {
         }
         handleNextClick =async ()=>{
             console.log("next");
-            if(this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
-
-            }else{
+            if(!(this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
             let url =
               `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=53313403d76f4152ab061ff17a6843f5&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
             this.setState({loading:true})
@@ -58,6 +70,8 @@ export default class News extends Component {
               page:this.state.page+1,
               loading:false
             });
+            // this.setState({page:this.state.page+1})
+            // this.updateNew();
         }
     }
   render() {
@@ -75,14 +89,14 @@ export default class News extends Component {
                     title={elem.title ? elem.title.slice(0, 45) : ""}
                     description={elem.description ? elem.description.slice(0, 88) : ""}
                     imgUrl={!elem.urlToImage?"https://cdn.ndtv.com/common/images/ogndtv.png":elem.urlToImage}
-                    newsUrl={elem.url}
+                    newsUrl={elem.url} author={!elem.author?"Unknown":elem.author} date={elem.publishedAt}  source={elem.source}
                   />
                 </div>
               );
             })}
           </div>
           <div className="d-flex justify-content-between">
-          <button disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Prev</button>
+          <button disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
           <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}> &rarr; Next</button>
           </div>
         </div>
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react'
 
 export default class NewsItems extends Component {
-    
+
   render() {
-    let { title, description, imgUrl,newsUrl } = this.props;
+    let { title, description, imgUrl, newsUrl, author, date ,source } = this.props;
     return (
       <div>
-        <div className="card my-2" style={{ width: "20rem" ,height:"25rem" }}>
-          <img src={imgUrl} className="card-img-top" alt="News Image" style={{ width: "20rem",height:"12rem" }} />
+        <div className="card my-2" style={{ width: "25rem", height: "30rem" }}>
+          <img
+            src={imgUrl}
+            className="card-img-top"
+            alt="News Image"
+            style={{ width: "25rem", height: "15rem" }}
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title} 
+            <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{zIndex:'1',left:'50%'}}>
+              {source.name}
+             </span></h5>
             <p className="card-text">{description}</p>
-            <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
+            <p className="card-text">
+              <small className="text-muted ">
+                By -{author} On {new Date(date).toGMTString()}
+              </small>
+            </p>
+
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
               Learn More
             </a>
           </div>
